6f05b447597f809179af9d45fea708fb8663373b
orekie <kear_des@qq.com>
2015-07-17 18:25:12 +0800

    7-17 18.25

--- app/src/main/java/com/printfstudio/current/ui/fragment/MyPageFragment.java
+++ app/src/main/java/com/printfstudio/current/ui/fragment/MyPageFragment.java
@@ -4,20 +4,24 @@ package com.printfstudio.current.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.printfstudio.current.R;
+import com.printfstudio.current.ui.activity.MainActivity;
 
 public class MyPageFragment extends Fragment {
     private View fragmentView;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         fragmentView = inflater.inflate(R.layout.fragment_mypage, container, false);
-
+        MainActivity mainActivity = (MainActivity) getActivity();
+        mainActivity.setToolbar((Toolbar) fragmentView.findViewById(R.id.toolbar));
         return fragmentView;
     }
 }
