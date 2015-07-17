3365bf5d7d4dbb8b7e53735cdbb7393c137da413
orekie <kear_des@qq.com>
2015-07-17 19:05:44 +0800

    7-17 19 05

--- app/src/main/java/com/printfstudio/current/ui/activity/MainActivity.java
+++ app/src/main/java/com/printfstudio/current/ui/activity/MainActivity.java
@@ -4,17 +4,20 @@ package com.printfstudio.current.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
 
 import com.printfstudio.current.R;
-import com.printfstudio.current.activity.Timeline;
+import com.printfstudio.current.ui.fragment.MyPageFragment;
 
 
 /**
  * 程序的入口Activity
  */
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private NavigationView navigationView = null;
+    private NavigationView navigationView;
+    private MyPageFragment myPageFragment = new MyPageFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         this.initActivity();
         initViews();
         initData();
+        changeToMyPage();
     }
 
 
@@ -34,15 +38,26 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     private void initData() {
     }
 
+    private void changeToMyPage() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_frame, myPageFragment);
+        transaction.commit();
+    }
+
+    private void changeToHallPage() {
+
+    }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         item.setChecked(true);
         switch (item.getItemId()) {
             case R.id.nav_myPage:
-                startActivity(new Intent(MainActivity.this, Timeline.class));
+                changeToMyPage();
                 break;
             case R.id.nav_home:
+                changeToHallPage();
                 break;
             case R.id.nav_newDiary:
                 startActivity(new Intent(MainActivity.this, CreateDiaryActivity.class));
